2689e2134a29682d3eaf0173736f649217fe76e3
Just R <remixingmagelang@gmail.com>
2025-01-30 07:15:50 +0700

    update sc.js

--- sc.js
+++ sc.js
@@ -2,17 +2,29 @@
 function updateTimeDate() {
     let now = new Date();
 
-    // Format: DD/MM/YYYY HH:mm:ss
-    let date = now.getDate().toString().padStart(2, '0') + '/' + 
-              (now.getMonth() + 1).toString().padStart(2, '0') + '/' + 
+    // Nama bulan
+    const bulan = [
+        "Januari", "Februari", "Maret", "April", "Mei", "Juni", 
+        "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+    ];
+
+    // Format tanggal: DD bulan YYYY
+    let date = now.getDate().toString().padStart(2, '0') + ' ' + 
+              bulan[now.getMonth()] + ' ' + 
               now.getFullYear();
 
+    // Format jam: HH:mm:ss
     let time = now.getHours().toString().padStart(2, '0') + ':' + 
                now.getMinutes().toString().padStart(2, '0') + ':' + 
                now.getSeconds().toString().padStart(2, '0');
 
-    // Gabungkan waktu dan tanggal
-    document.getElementById("time-date").innerHTML = `${time} ${date}`;
+    // Zona Waktu WIB
+    let wib = "WIB (UTC+7)";
+
+    // Menampilkan tanggal, jam, dan WIB
+    document.getElementById("tanggal").innerHTML = `<b>Tanggal:</b> ${date}`;
+    document.getElementById("jam").innerHTML = `<b>Jam:</b> ${time}`;
+    document.getElementById("wib").innerHTML = wib;
 }
 
 // Update waktu dan tanggal setiap detik
